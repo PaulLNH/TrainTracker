a0adbaacbaffdb638f78e9c7909ce3dede762dd0
paullnh <plaird83@gmail.com>
2018-04-22 17:07:41 -0400

    Train tracking app working with static data. (does not refresh every min yet)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,4 +9,118 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var dataRef = firebase.database();
+var database = firebase.database();
+
+var train = [];
+
+// 2. Adding new train
+$("#submitBtn").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs user input
+  var trainName = $("#inputName")
+    .val()
+    .trim();
+  var trainDestination = $("#inputDestination")
+    .val()
+    .trim();
+  var trainStart = moment(
+    $("#inputFirst")
+      .val()
+      .trim(),
+    "HHmm"
+  ).format("HHmm");
+  var trainFrequency = $("#inputFrequency")
+    .val()
+    .trim();
+
+  // Creates local "temporary" object for holding train data
+  var newTrain = {
+    name: trainName,
+    destination: trainDestination,
+    start: trainStart,
+    frequency: trainFrequency
+  };
+
+  // Uploads employee data to the database
+  database.ref().push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.start);
+  console.log(newTrain.frequency);
+
+  // Alert
+  alert("Train successfully added");
+
+  // Clears all of the text-boxes
+  $("#inputName").val("");
+  $("#inputDestination").val("");
+  $("#inputFirst").val("");
+  $("#inputFrequency").val("");
+});
+
+// 3. Create Firebase event for adding new train to the database and a row to our table
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().destination;
+  var trainStart = childSnapshot.val().start;
+  var trainFrequency = childSnapshot.val().frequency;
+
+  ///////////// TEST CODE //////////
+
+  /////////////// TEST CODE //////////
+
+  // Calculate the train times using moment js
+
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var startTimeConverted = moment(trainStart, "HH:mm");
+  var displayTime = moment(startTimeConverted).format("HH:mm");
+  console.log("START TIME: " + displayTime);
+
+  // Current Time
+  var currentTime = moment();
+  // Use this variable to change the time at the top of the chart for CURRENT TIME:
+  var displayCurrent = moment(currentTime).format("HH:mm");
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(startTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % trainFrequency;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var tMinutesTillTrain = trainFrequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+
+  var displayArrival = moment(nextTrain).format("HH:mm");
+  // TEST CODE ABOVE THIS LINE
+
+  $("#currentTime").text(displayCurrent);
+  // Add each train's data into the table
+  $("#trainTable").append(
+    "<tr><td>" +
+      trainName +
+      "</td><td>" +
+      trainDestination +
+      "</td><td>" +
+      displayTime +
+      "</td><td>" +
+      trainFrequency +
+      " min</td><td>" +
+      displayArrival +
+      "</td><td>" +
+      tMinutesTillTrain +
+      " min </td></tr>"
+  );
+});
