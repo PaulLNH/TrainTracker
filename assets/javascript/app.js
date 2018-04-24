63baf1d136ef5754c92b012cca5b3dae6704f9a4
paullnh <plaird83@gmail.com>
2018-04-24 18:29:58 -0400

    Testing oauth google account signin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,47 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// Login with Google
+ui.start("#firebaseui-auth-container", {
+  signInOptions: [
+    // List of OAuth providers supported.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ]
+  // Other config options...
+});
+
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById("loader").style.display = "none";
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: "popup",
+  signInSuccessUrl: "https://paullnh.github.io/TrainTracker/index.html",
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: "https://paullnh.github.io/TrainTracker/index.html"
+};
+
+// The start method will wait until the DOM is loaded.
+ui.start("#firebaseui-auth-container", uiConfig);
+////// OAuth goes above here //////
+
 // Store database obj to var
 var database = firebase.database();
 
