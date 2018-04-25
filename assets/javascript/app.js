f91191d71dbf088ef85f2b52128fc84802a72cbb
paullnh <plaird83@gmail.com>
2018-04-24 22:11:52 -0400

    Auth testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,17 +37,22 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: "popup",
-  signInSuccessUrl: "https://paullnh.github.io/TrainTracker/index.html",
+  signInSuccessUrl: "./index.html",
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
-  tosUrl: "https://paullnh.github.io/TrainTracker/index.html"
+  tosUrl: "./index.html"
 };
 
 // The start method will wait until the DOM is loaded.
 ui.start("#firebaseui-auth-container", uiConfig);
+
+if (user) {
+  console.log("You're in!");
+}
+
 ////// OAuth goes above here //////
 
 // Store database obj to var
